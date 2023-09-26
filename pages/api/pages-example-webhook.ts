47479bd3fa0cf0b7517c27ddb7b402a62c3bf8e7
Payton Boxleitner <pboxleitner@outlook.com>
2023-09-25 22:29:51 -0700

    setup page router webhook

---
+++ pages/api/pages-example-webhook.ts
@@ -0,0 +1,21 @@
+// packages
+import twilio from "twilio";
+
+// types
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// constants
+const { MessagingResponse } = twilio.twiml;
+
+type ResponseData = {
+  message: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const twiml = new MessagingResponse();
+  twiml.message("This is a test");
+  res.status(200).send(twiml.toString());
+}
