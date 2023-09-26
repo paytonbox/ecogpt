fb5c94465cfb70048fdba312bb8eb73ba9c2b829
Payton Boxleitner <pboxleitner@outlook.com>
2023-09-25 21:41:18 -0700

    add twilio response to serverless function

--- app/api/example/route.ts
+++ app/api/example/route.ts
@@ -1,9 +1,21 @@
+// packages
 import { NextResponse } from "next/server";
+import twilio from "twilio";
+
+// types
 import type { NextRequest } from "next/server";
+
+// constants
+const { MessagingResponse } = twilio.twiml;
+
 export function GET(request: NextRequest) {
+  const twiml = new MessagingResponse();
+
+  twiml.message("This is a test");
+
   return NextResponse.json(
     {
-      body: request.body,
+      body: twiml.toString(),
       path: request.nextUrl.pathname,
       query: request.nextUrl.search,
       cookies: request.cookies.getAll(),
